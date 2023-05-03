6bf6db975511c12dbf853e90509ea6b05c2e52be
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-02 22:16:29 -0600

    final commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matches = list()
+        for word in words:
+            letters = [letter for letter in word]
+            if sorted(letters) == sorted(self.word):
+                matches.append(word)
+        return matches
